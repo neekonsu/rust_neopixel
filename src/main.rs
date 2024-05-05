bd17e18759b881ef2ed33c74f7d8d97c0acd72eb
Neekon Saadat <neekon@berkeley.edu>
2024-05-04 21:10:47 -0700

    clarifying comments

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![no_main]
 
 use cortex_m_rt::entry;
-use panic_halt as _;
+use panic_halt as _; /* implements default panic due to #![no_std] macro */
 
 #[entry] /* Manual entrypoint annotation due to #![no_main] macro */
 fn main() -> !
